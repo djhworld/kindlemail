4c3093b7d5879c1eb8b7ef38b7358197ad3cafe6
Daniel Harper <djharperuk@gmail.com>
2011-01-09 15:22:48 +0000

    Bugfixes with CLI application

--- lib/KindleMailer.rb
+++ lib/KindleMailer.rb
@@ -9,16 +9,16 @@ class KindleMailer
 
   def send(kindle_address, file)
     @kindle_address = kindle_address
-    raise ArgumentError, "The file you have specified does not exist #{SEE_HELP}" if file.nil? || !File.exist?(file)
+    filepath = File.expand_path(file)
+    raise ArgumentError, "The file you have specified does not exist #{SEE_HELP}" if file.nil? || !File.exist?(filepath)
     raise ArgumentError, "The file you have specified is not a valid type #{SEE_HELP}" if VALID_FILE_TYPES.include?(File.extname(file)) == false
 
-    filepath = File.expand_path(file)
-    puts "Preparing #{File.basename(file)} to be sent to #{@kindle_address}"
+    puts "Preparing #{File.basename(filepath)} to be sent to #{@kindle_address}"
 
     mailer = GmailMailer::Mailer.new(@email_credentials)
     mailer.sendMessage(GmailMailer::Message.new(@kindle_address, "a@b.c", filepath))
 
-    puts "#{File.basename(file)} was successfully sent to #{@kindle_address}"
+    puts "#{File.basename(filepath)} was successfully sent to #{@kindle_address}"
     return true
   end
 end 
